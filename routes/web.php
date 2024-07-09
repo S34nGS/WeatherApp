f7695596904c074d795306d20c658ea05f884d7b
S34nGS <steenhuis20@gmail.com>
2024-07-09 21:05:25 +0200

    first api try

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,15 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    $location = 'Rotterdam';
+    $apiKey = '14ac00775cb76560a48806084b5394ef';
+
+    $response = Http::get("https://api.openweathermap.org/data/2.5/weather?q={$location}&appid={$apiKey}&units=metric");
+    // $response = Http::get("https://api.openweathermap.org/data/2.5/forecast/daily?q={$location}&cnt=5&appid={$apiKey}&units=metric");
+
+    dump($response->json());
+
+    return view('welcome', [
+        'currentWeather' => $response->json(),
+    ]);
 });
