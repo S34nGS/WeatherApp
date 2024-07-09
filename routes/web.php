ba11e466c1211179dbd27d08d186f47a326080b3
S34nGS <steenhuis20@gmail.com>
2024-07-09 21:55:40 +0200

    added the api key in an environment variable

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     $location = 'Rotterdam';
-    $apiKey = '14ac00775cb76560a48806084b5394ef';
+    $apiKey = config('services.weather.key');
 
     $response = Http::get("https://api.openweathermap.org/data/2.5/weather?q={$location}&appid={$apiKey}&units=metric");
     // $response = Http::get("https://api.openweathermap.org/data/2.5/forecast/daily?q={$location}&cnt=5&appid={$apiKey}&units=metric");
