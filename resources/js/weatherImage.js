61239d3358dd8b8c82e69c65c23f6610ebe324e0
S34nGS <steenhuis20@gmail.com>
2024-07-24 18:26:07 +0300

    added images and routing

--- resources/js/weatherImage.js
+++ resources/js/weatherImage.js
@@ -3,11 +3,15 @@ document.addEventListener("DOMContentLoaded", function(){
     console.log('Weather condition:', condition);
 
     var conditionToImage = {
-        "Sunny": "images/sunny.png",
-        "Partly cloudy": "images/cloudy.png",
+        "Sunny": "images/PNG/256/day_clear.png",
+        "Partly cloudy": "images/PNG/256/day_partial_cloud.png",
+        "Cloudy": "images/PNG/256/cloudy.png",
+        "Overcast": "images/PNG/256/overcast.png",
+        "Mist": "images/PNG/256/mist.png",
+        "Fog": "images/PNG/256/fog.png",
     };
 
-    var imageURL = conditionToImage[condition] || "images/thunder.png";
+    var imageURL = conditionToImage[condition] || "images/PNG/256/day_clear.png";
     console.log('Image URL:', imageURL);    
 
     var weatherIconDiv = document.getElementById('weathericon');
--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,4 @@
 use App\Http\Controllers\WeatherApi;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/weather', [WeatherApi::class, 'fetchData'])->name('weather.fetch');
+Route::get('/', [WeatherApi::class, 'fetchData'])->name('weather.fetch');
