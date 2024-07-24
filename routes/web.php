da8d46841f121f954c5ba1cc1fc2f68f07d0ea63
S34nGS <steenhuis20@gmail.com>
2024-07-24 19:57:48 +0300

    test page for apache server

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,7 @@ use App\Http\Controllers\WeatherApi;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [WeatherApi::class, 'fetchData'])->name('weather.fetch');
+
+Route::get('/test', function() {
+    return view('welcome');
+});
