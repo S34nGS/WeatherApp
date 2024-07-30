5954469a072fd904962374bbb10e1c6de2380e4e
S34nGS <steenhuis20@gmail.com>
2024-07-30 22:01:32 +0300

    vite for server

--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,11 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    server: {
+        host: '0.0.0.0', // Allows external access
+        port: 5173,
+        hmr: {
+          host: '194.61.65.208', // Replace with your server's IP address
+        },
+      },
 });
