561ec4d661845885584fb57b49c70d52dfcfacd7
S34nGS <steenhuis20@gmail.com>
2024-07-30 21:52:08 +0300

    test revert

--- resources/views/weather.blade.php
+++ resources/views/weather.blade.php
@@ -22,7 +22,7 @@
             <tbody>
                 <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
                     <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">Location</th>
-                    <td id="test" class="px-6 py-4">{{ $data['location']['name'] }}</td>
+                    <td class="px-6 py-4">{{ $data['location']['name'] }}</td>
                 </tr>
 
                 <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
