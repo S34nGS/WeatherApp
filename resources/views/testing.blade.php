5ac07c5bf4a032441ae483e33af22bfb716abe7d
S34nGS <steenhuis20@gmail.com>
2024-08-04 15:44:00 +0200

    save before auth

--- resources/views/testing.blade.php
+++ resources/views/testing.blade.php
@@ -42,6 +42,47 @@
               </div>
           </div>
       </div>
-  </div> 
+  </div>
+
+
+
+
+
+
+
+
+
+
+
+
+<div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+    <table class="w-full text-sm text-left rtl:text-right text-blue-100 dark:text-blue-100">
+        <thead class="text-xs text-white uppercase bg-blue-600 dark:text-white">
+            <tr>
+                <th scope="col" class="px-6 py-3">Attribute</th>
+                <th scope="col" class="px-6 py-3">Value</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr class="bg-blue-500 border-b border-blue-400">
+                <th scope="row" class="px-6 py-4 font-medium text-blue-50 whitespace-nowrap dark:text-blue-100">
+                    Apple MacBook Pro 17"
+                </th>
+                <td class="px-6 py-4">
+                    Silver
+                </td>
+            </tr>
+            <tr class="bg-blue-500 border-b border-blue-400">
+                <th scope="row" class="px-6 py-4 font-medium text-blue-50 whitespace-nowrap dark:text-blue-100">
+                    Microsoft Surface Pro
+                </th>
+                <td class="px-6 py-4">
+                    White
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+
   
 </x-layout>
--- resources/views/weather.blade.php
+++ resources/views/weather.blade.php
@@ -19,6 +19,7 @@
                 <th scope="col" class="px-6 py-3">Attribute</th>
                 <th scope="col" class="px-6 py-3">Value</th>
             </thead>
+            
             <tbody>
                 <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
                     <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">Location</th>
